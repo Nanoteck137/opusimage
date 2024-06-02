cb1f75795e795c894de909b685560854af6dbbe8
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-06-02 14:36:22 +0200

    Added some tag editing

--- opusimage.py
+++ opusimage.py
@@ -6,26 +6,58 @@ from mutagen.flac import Picture
 from base64 import b64encode
 
 import argparse
+import os
 
 parser = argparse.ArgumentParser(prog = "opusimage")
 parser.add_argument("opusfile")
-parser.add_argument("imagefile")
+parser.add_argument("--image")
+parser.add_argument("--title")
+parser.add_argument("--album")
+parser.add_argument("--artist")
+parser.add_argument("--remove", action="store_true")
 
-def add_image(audio_path, image_path):
+def get_mime(ext):
+    if ext == ".png":
+        return "image/png"
+
+    if ext == ".jpeg" or ext == ".jpg":
+        return "image/jpeg"
+    
+    raise Exception("Unsupported ext: " + ext)
+
+def add_image(audio: OggOpus, image_path):
+    _, ext = os.path.splitext(image_path)
+
     coverart = Picture()
-    coverart.mime = "image/jpeg"
+    coverart.mime = get_mime(ext)
+
     with open(image_path, 'rb') as f:
         coverart.data = f.read()
     coverart.type = 3
 
-    audio = OggOpus(audio_path)
     audio['metadata_block_picture'] = b64encode(coverart.write()).decode('ascii')
-
-    audio.save()
 
 def main():
     args = parser.parse_args()
-    add_image(args.opusfile, args.imagefile)
+
+    audio = OggOpus(args.opusfile)
+
+    if args.remove:
+        audio.delete()
+
+    if args.image:
+        add_image(audio, args.image)
+
+    if args.title:
+        audio['title'] = args.title
+
+    if args.album:
+        audio['album'] = args.album
+        
+    if args.artist:
+        audio['artist'] = args.artist
+
+    audio.save()
 
 if __name__ == "__main__":
     main()
